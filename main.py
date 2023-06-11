818128b8284ec0da5151f218e196daafb5bda334
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2023-06-11 16:14:52 +0200

    Further fixes and optimalization after running halvedgridsearch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import sklearn
 import pandas as pd
@@ -11,21 +12,24 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.decomposition import TruncatedSVD
 
-params = {'max_depth': [20, 25, 30, 35, 40, 45, 50],
-          'min_samples_split': [2, 5, 10],
-          'min_samples_leaf': [1, 2, 4]}
+
+
+
+params = {'max_depth': [10, 50, 100, 200]}
+
+
 
 dataset = pd.read_csv("C:/Users/Bartek/Desktop/Datasets/processed_train_dataset.csv")
 
 y = dataset['log_price']
-
+start_time = datetime.datetime.now()
 encoded_brand_name = OneHotEncoder(handle_unknown='ignore')
 encoded_brand_name.fit(dataset.brand_name.values.reshape(-1, 1))
 train_encoded_brand_name = encoded_brand_name.transform(dataset.brand_name.values.reshape(-1, 1))
 
-svd = TruncatedSVD(n_components=100)
-sparse_feature_transformed = svd.fit_transform(train_encoded_brand_name)
-print(sparse_feature_transformed.shape)
+#svd = TruncatedSVD(n_components=100)
+#sparse_feature_transformed = svd.fit_transform(train_encoded_brand_name)
+#print(sparse_feature_transformed.shape)
 
 
 def log_to_actual(log):
@@ -33,12 +37,12 @@ def log_to_actual(log):
 
 
 # Applying TfIdf to item description
-tfidf_description = TfidfVectorizer(ngram_range=(1, 2), max_features=100)
+tfidf_description = TfidfVectorizer(ngram_range=(1, 2), max_features=50000)
 tfidf_vec_description = tfidf_description.fit_transform(dataset.item_description.values.astype('U'))
 print(tfidf_vec_description.shape)
 
 # Applying TfIdf to name
-tfidf_name = TfidfVectorizer(ngram_range=(1, 2), max_features=100)
+tfidf_name = TfidfVectorizer(ngram_range=(1, 2), max_features=50000)
 tfidf_vec_name = tfidf_name.fit_transform(dataset.name.values.astype('U'))
 
 encoded_category_tier1 = OneHotEncoder(handle_unknown='ignore')
@@ -66,16 +70,28 @@ dataset_vector = hstack(
     (tfidf_vec_description, tfidf_vec_name, train_encoded_tier1, train_encoded_tier2, train_encoded_tier3,
      train_encoded_shipping,
      train_encoded_item_condition, train_encoded_brand_name))
+print(dataset_vector.shape)
 
 vector_train, vector_validation, y_train, y_validation = sklearn.model_selection.train_test_split(dataset_vector, y,
                                                                                                   test_size=0.3,
                                                                                                   random_state=3)
-print("Hiperparametry drzewa decyzyjnego")
-dt = DecisionTreeRegressor()
-grid_dt = HalvingGridSearchCV(dt, param_grid=params, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
-grid_dt.fit(vector_train, y_train)
-print("Best Hyperparameters:", grid_dt.best_params_)
+#print("Hiperparametry drzewa decyzyjnego")
+#dt = DecisionTreeRegressor(criterion="squared_error", max_features="auto")
+#grid_dt = HalvingGridSearchCV(dt, param_grid=params, cv=3, return_train_score=True, scoring='neg_mean_squared_error', n_jobs=-1)
+#grid_dt.fit(vector_train, y_train)
+#print("Best Hyperparameters:", grid_dt.best_params_)
+end_time = datetime.datetime.now()
+runtime = end_time - start_time
+total_seconds = runtime.total_seconds()
 
+hours = int(total_seconds // 3600)
+minutes = int((total_seconds % 3600) // 60)
+seconds = int(total_seconds % 60)
+formatted_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+# Wydruk wyniku
+print("Czas wykonania tworzenia macierzy rzadkiej: " + formatted_time)
+
+start_time = datetime.datetime.now()
 print("Rozpoczynam tworzenie modelu Regresji liniowej")
 linear_regression = LinearRegression(copy_X=True, fit_intercept=False, n_jobs=-1)
 print("Tworzenie modelu testowego")
@@ -88,17 +104,28 @@ linear_regression_validation_predictions = linear_regression.predict(vector_vali
 
 train_error = np.sqrt(
     mean_squared_error(y_train, linear_regression_train_predictions))  # Calculating RMSLE of Linear Regression model
-print("Błąd danych treningowych:", train_error)
+print("Błąd danych treningowych dla modelu Regresji Liniowej:", train_error)
 
 r2train = r2_score(y_train, linear_regression_train_predictions)
-print("Wynik r2:", r2train)
+print("Wynik r2 dla modelu Regresji Liniowej: ", r2train)
 
 validation_error = np.sqrt(mean_squared_error(y_validation, linear_regression_validation_predictions))
-print("Błąd danych walidacyjnych:", validation_error)
-print("R2 Validation:", r2_score(y_validation, linear_regression_validation_predictions))
+print("Błąd danych walidacyjnych dla modelu Regresji Liniowej:", validation_error)
+print("R2 Validation dla modelu Regresji Liniowej:", r2_score(y_validation, linear_regression_validation_predictions))
+end_time = datetime.datetime.now()
+runtime = end_time - start_time
+total_seconds = runtime.total_seconds()
 
+hours = int(total_seconds // 3600)
+minutes = int((total_seconds % 3600) // 60)
+seconds = int(total_seconds % 60)
+formatted_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+# Wydruk wyniku
+print("Czas wykonania trenowania i predykcji dla modelu regresji liniowej: " + formatted_time)
+
+start_time = datetime.datetime.now()
 print("Rozpoczynam pracę nad modelem drzewa regresyjnego")
-dt = DecisionTreeRegressor(max_depth=30, min_samples_leaf=4, min_samples_split=10)
+dt = DecisionTreeRegressor(max_depth=30, max_features='auto')
 dt.fit(vector_train, y_train)
 print("Rozpoczynam predykcje na danych treningowych")
 dt_train_predictions = dt.predict(vector_train)
@@ -106,10 +133,20 @@ print("Rozpoczynam predykcje na danych walidacyjnych")
 dt_validation_predictions = dt.predict(vector_validation)
 
 train_error_dt = np.sqrt(mean_squared_error(y_train, dt_train_predictions))  # Calculating RMSLE of Regression tree
-print("Błąd danych treningowych:", train_error_dt)
+print("Błąd danych treningowych dla modelu Drzewa Regresyjnego:", train_error_dt)
 r2train_dt = r2_score(y_train, dt_train_predictions)
 print("Wynik r2:", r2train_dt)
 
 validation_error_dt = np.sqrt(mean_squared_error(y_validation, dt_validation_predictions))
-print("Błąd danych walidacyjnych:", validation_error_dt)
+print("Błąd danych walidacyjnych dla modelu Drzewa Regresyjnego:", validation_error_dt)
 print("R2 Validation:", r2_score(y_validation, dt_validation_predictions))
+end_time = datetime.datetime.now()
+runtime = end_time - start_time
+total_seconds = runtime.total_seconds()
+
+hours = int(total_seconds // 3600)
+minutes = int((total_seconds % 3600) // 60)
+seconds = int(total_seconds % 60)
+formatted_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+# Wydruk wyniku
+print("Czas wykonania trenowania i predykcji dla modelu drzewa regresyjnego: " + formatted_time)
