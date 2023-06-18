2d0a7862bf5fa3137074fe326502e004e37b3964
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2023-06-18 23:53:37 +0200

    Data Analysys for thesis

--- DataAnalasys.py
+++ DataAnalasys.py
@@ -2,8 +2,9 @@ import pandas
 import matplotlib.pyplot as plt
 import seaborn as snp
 import numpy as np
+from matplotlib import ticker
 
-plt.rcParams['font.size'] = 30
+plt.rcParams['font.size'] = 20
 pandas.set_option('display.float_format', '{:.2f}'.format)
 df = pandas.read_csv("C:/Users/Bartek/Desktop/datasets/train.tsv", sep='\t')
 df['brand_name'][df.brand_name.isnull()] = "missing"
@@ -13,6 +14,8 @@ print(df.info())
 print(df.describe())
 df["log_price"] = df.price.apply(lambda x:  np.log(x+1))
 print(df.name.describe())
+formatter = ticker.ScalarFormatter()
+formatter.set_scientific(False)
 
 
 print(df['price'].describe().apply("{0:.5f}".format))  # Najważniejsze cechy kolumny Price
@@ -22,6 +25,14 @@ df['category_name'][df.category_name.isnull()] = "missing"
 df['Tier_1'] = df.category_name.apply(lambda x:    x.split("/")[0] if len(x.split("/"))>=1 else "missing")
 df['Tier_2'] = df.category_name.apply(lambda x:    x.split("/")[1] if len(x.split("/"))>1 else "missing")
 df['Tier_3'] = df.category_name.apply(lambda x:    x.split("/")[2] if len(x.split("/"))>1 else "missing")
+
+plt.figure(figsize=(20,15))
+df_brand_name_sorted = df[["brand_name", "price"]].groupby(by="brand_name").max()
+df_brand_name_sorted = df_brand_name_sorted.sort_values(by="price", ascending=False)
+snp.barplot(x=df_brand_name_sorted.index[:10],y= df_brand_name_sorted.price[:10])
+plt.title('Top 10 most expensive brand names')
+plt.xticks(rotation=45)
+plt.show()
 
 #Print the most common categories in each tier with it's count
 print(df.Tier_1.value_counts().index[:10])
@@ -45,9 +56,6 @@ print(df.shipping.value_counts().values)
 #Print item_condition distribution
 print(df.item_condition_id.value_counts().index)
 print(df.item_condition_id.value_counts().values)
-
-
-
 
 #Plot the item_condition distribution
 plt.figure(figsize=(20,15))
@@ -88,33 +96,31 @@ plt.hist(df.log_price,bins=30,color="teal")
 plt.title("Log(Price+1) Distribution")
 plt.xlabel("log(Price+1)")
 plt.ylabel("Count")
-plt.title("Log price distribution")
 plt.show()
 
 plt.figure(figsize=(20, 15))
 plt.hist(df.price, bins=30, label='Price')
 plt.title("Price distribution")
-plt.xlabel('Price', fontsize=15)
+plt.xlabel('Price')
 plt.ylabel('Amount')
-plt.xticks(fontsize=15)
-plt.yticks(fontsize=15)
-plt.legend(fontsize=15)
+plt.ticklabel_format(style='plain')
 plt.show()
 
 #Analyzing mean and median value for Tier1 categories
+grouped_df = df.groupby('Tier_1')['price']
+plt.figure(figsize=(20,15))
+snp.barplot(y=grouped_df['price'].values, x=grouped_df["Tier_1"])
+plt.title("Tier1 vs Price")
+plt.xlabel("Tier1 category")
+plt.xticks(rotation=45)
+plt.show()
+
 grouped_df = df.groupby('Tier_1')['price'].mean().reset_index()
 plt.figure(figsize=(20,15))
 snp.barplot(y=grouped_df['price'].values, x=grouped_df["Tier_1"])
 plt.title("Tier1 vs Price")
 plt.xlabel("Tier1 category")
-plt.show()
-
-#Analyzing mean and median value for Tier1 categories
-grouped_df = df.groupby('Tier_1')['price'].median().reset_index()
-plt.figure(figsize=(20,15))
-snp.barplot(y=grouped_df['price'], x=grouped_df["Tier_1"])
-plt.title("Tier1 vs Price")
-plt.xlabel("Tier1 category")
+plt.xticks(rotation=45)
 plt.show()
 
 # Price distribution looked at via shipping
@@ -123,10 +129,8 @@ bins = 50
 plt.hist(df[df['shipping']==1]['price'],bins, density=True, range=[0,250], alpha=0.6, label='Price when the seller paid for the shipping')
 plt.hist(df[df['shipping']==0]['price'], bins, density=True, range=[0,250], alpha=0.6, label='Price when the buyer paid for the shipping')
 plt.title("Shipping vs Price")
-plt.xlabel('Price', fontsize=15)
-plt.legend(fontsize=15)
-plt.xticks(fontsize=15)
-plt.yticks(fontsize=15)
+plt.xlabel('Price')
+plt.ticklabel_format(style='plain')
 plt.show()
 
 #Item condition vs median price
@@ -137,13 +141,28 @@ plt.show()
 
 #Analiza najdroższych brandów w danych trenningowych
 plt.figure(figsize=(20,15))
-snp.barplot(x='brand_name', y='price', data=df.groupby('brand_name').price.mean().nlargest(10).reset_index())
-plt.title('Brand_name vs Price')
-plt.xticks(rotation=70)
+df_brand_name_sorted = df[["brand_name", "price"]].groupby(by="brand_name").max()
+df_brand_name_sorted = df_brand_name_sorted.sort_values(by="price", ascending=False)
+snp.barplot(x=df_brand_name_sorted.index[:10],y= df_brand_name_sorted.price[:10])
+plt.title('Top 10 most expensive brand names')
+plt.xticks(rotation=45)
 plt.show()
 
-# Rozkład cen produktów w zależności od jakośći podanej przez sprzedającego
-df = df[df['price']<250]
+plt.figure(figsize=(20, 15))
+df_brand_name_sorted = df[["brand_name", "price"]].groupby(by="brand_name").mean()
+df_brand_name_sorted = df_brand_name_sorted.sort_values(by="price", ascending=False)
+snp.barplot(x=df_brand_name_sorted.index[:10], y=df_brand_name_sorted.price[:10])
+plt.title('Brand_name vs Price')
+plt.xticks(rotation=45)
+plt.show()
+
+#Shipping vs price boxplot
+plt.figure(figsize=(20,15))
+snp.boxplot(x='shipping', y='log_price', data=df)
+plt.title("Shipping vs log_price")
+plt.show()
+
+# Item_condition vs price
 my_plot = []
 for i in df['item_condition_id'].unique():
     my_plot.append(df[df['item_condition_id']==i]['log_price'])
@@ -153,16 +172,28 @@ colors = ['cyan', 'lightblue', 'lightgreen', 'tan', 'pink']
 for patch, color in zip(bp['boxes'], colors):
     patch.set_facecolor(color)
 axes.yaxis.grid(True)
-
-plt.title('BoxPlot price X item_condition_id', fontsize=15)
-plt.xlabel('item_condition_id', fontsize=15)
-plt.ylabel('log_price', fontsize=15)
-plt.xticks(fontsize=15)
-plt.yticks(fontsize=15)
+plt.title('BoxPlot log_price X item_condition_id')
+plt.xlabel('item_condition_id')
+plt.ylabel('log_price')
 plt.show()
 
 #Analyzing mean and median value for Tier1 categories
 plt.figure(figsize=(20,15))
-snp.barplot(x='Category', y='Mean price', data=df[["price","Tier1"]].groupby(by='Tier1').mean().values)
+snp.barplot(x='Category', y='log_price', data=df)
+plt.title("Tier 1 log_price distribution")
+plt.show()
+plt.figure(figsize=(20,15))
+snp.barplot(x='Category', y='Mean price', data=df[["price","Tier_1"]].groupby(by='Tier_1').mean().values)
+plt.title("Mean Tier_1 log_price distribution")
+plt.show()
+
+
+# Boxplot Tier1 vs log_price
+plt.figure(figsize=(20, 15))
+snp.boxplot(x='Tier_1', y='log_price', data=df)
+plt.ylabel('Log Price')
+plt.xlabel('Tier_1')
+plt.title('BoxPlot Log Price by Tier_1')
+plt.xticks(rotation=45)
 plt.show()
 
